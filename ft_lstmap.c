2c3f186df21d69ba71e3bf3a768aa6cea92d4182
ricso47 <lucamihace@gmail.com>
2018-01-18 15:05:15 +0200

    done

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmihance <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/18 14:27:28 by lmihance          #+#    #+#             */
+/*   Updated: 2018/01/18 14:27:52 by lmihance         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*new_lst;
+	t_list	*new_elem;
+	t_list	*prev_elem;
+
+	new_lst = NULL;
+	if (lst && (*f))
+	{
+		new_lst = (*f)(lst);
+		prev_elem = new_lst;
+		lst = lst->next;
+		while (lst)
+		{
+			new_elem = (*f)(lst);
+			prev_elem->next = new_elem;
+			prev_elem = new_elem;
+			lst = lst->next;
+		}
+		prev_elem->next = NULL;
+	}
+	return (new_lst);
+}
